13597483263ef09ab54c3dd69f6da49e42f7a157
weiwongfaye <weiwongfaye@gmail.com>
2015-03-02 10:22:10 +0800

    add feed parser

---
+++ multi_computing/queue_for_rss_feed.py
@@ -0,0 +1,56 @@
+# System modules
+from Queue import Queue
+from threading import Thread
+import time
+
+# Local modules
+import feedparser
+
+# Set up some global variables
+num_fetch_threads = 3
+enclosure_queue = Queue()
+
+# A real app wouldn't use hard-coded data...
+feed_urls = [ 
+		'http://rss.cnn.com/rss/cnn_world.rss',
+             ]
+
+
+def downloadEnclosures(i, q):
+    """This is the worker thread function.
+    It processes items in the queue one after
+    another.  These daemon threads go into an
+    infinite loop, and only exit when
+    the main thread ends.
+    """
+    while True:
+        print '%s: Looking for the next enclosure' % i
+        summary = q.get()
+        print '%s: Downloading:' % i, summary
+        # instead of really downloading the URL,
+        # we just pretend and sleep
+        time.sleep(i + 2)
+        q.task_done()
+
+
+# Set up some threads to fetch the enclosures
+for i in range(num_fetch_threads):
+    worker = Thread(target=downloadEnclosures, args=(i, enclosure_queue,))
+    worker.setDaemon(True)
+    worker.start()
+
+# Download the feed(s) and put the enclosure URLs into
+# the queue.
+for url in feed_urls:
+    response = feedparser.parse(url, agent='fetch_podcasts.py')
+    for entry in response['entries']:
+        # for enclosure in entry.get('summary', []):
+        #     print 'Queuing:', enclosure['url']
+        #     enclosure_queue.put(enclosure['url'])
+        print 'Queuing:', entry['summary']
+        enclosure_queue.put(entry['summary'])
+# Now wait for the queue to be empty, indicating that we have
+# processed all of the downloads.
+print '*** Main thread waiting'
+enclosure_queue.join()
+print '*** Done'
