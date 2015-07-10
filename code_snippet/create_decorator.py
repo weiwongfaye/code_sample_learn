9cc6a8f11aab23057852874cc0319c56b7e4a353
Jack Wang <jackw@splunk.com>
2015-07-10 21:51:23 +0800

    add case: create decorator

---
+++ code_snippet/create_decorator.py
@@ -0,0 +1,23 @@
+def p_decorate(func):
+   def func_wrapper(name):
+       return "<p>{0}</p>".format(func(name))
+   return func_wrapper
+
+def strong_decorate(func):
+    def func_wrapper(name):
+        return "<strong>{0}</strong>".format(func(name))
+    return func_wrapper
+
+def div_decorate(func):
+    def func_wrapper(name):
+        return "<div>{0}</div>".format(func(name))
+    return func_wrapper
+
+
+@div_decorate
+@p_decorate
+@strong_decorate
+def get_text(name):
+   return "lorem ipsum, {0} dolor sit amet".format(name)
+
+print get_text("John")
