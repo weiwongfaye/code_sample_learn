b498b98722c36319648122cfbb6a4469abff1092
weiwongfaye <weiwongfaye@gmail.com>
2016-01-02 13:09:37 +0800

    cPickle moudle

---
+++ code_snippet/pickle_test.py
@@ -0,0 +1,14 @@
+import cPickle
+
+my_data = {"language":"python","verision":"2.7"}
+fp = open('picklefile.dat','wb')
+cPickle.dump(my_data,fp)
+fp.close()
+
+
+fp = open("picklefile.dat",'rb')
+out = cPickle.load(fp)
+print out
+print out['language']
+print out['verision']
+fp.close()
