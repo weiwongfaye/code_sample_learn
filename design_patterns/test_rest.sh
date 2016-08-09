5152e30564b097b6e0ccb4e8680638dad50c49d3
weiwongfaye <weiwongfaye@gmail.com>
2016-08-09 15:49:40 +0800

    add spin

---
+++ design_patterns/test_rest.sh
@@ -0,0 +1,6 @@
+curl -k -u admin:notchangeme https://10.66.129.225:8089/services/cluster/master/control/control/resync_bucket_from_peer -X POST -d bucket_id=_audit~2~8F6747E9-88C9-4488-8806-4EA3CA433CF5 -d peer=8F6747E9-88C9-4488-8806-4EA3CA433CF5
+
+curl -k -u admin:notchangeme https://10.66.129.225:8089/services/cluster/master/control/control/resync_bucket_from_peer -X POST -d bucket_id=_audit~2~8F6747E9-88C9-4488-8806-4EA3CA433CF5 -d peer=7969A54A-CDB0-4278-940A-63BBEFBEDA06
+
+curl -k -u admin:notchangeme https://10.66.129.225:8089/services/cluster/master/control/control/resync_bucket_from_peer -X POST -d bucket_id=_audit~2~8F6747E9-88C9-4488-8806-4EA3CA433CF5 -d peer=DFD6D4AC-AE1E-4E91-89AA-EE404A0AB22C
+
