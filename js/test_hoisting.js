5bd5afbfc6baec4ab7359ff56014a07e610de09e
weiwongfaye <weiwongfaye@gmail.com>
2016-10-17 17:28:42 +0800

    js hoisting example

---
+++ js/test_hoisting.js
@@ -0,0 +1,20 @@
+// Test1 - ReferenceError: productId is not defined
+//console.log(productId)
+// Test2 - undefined
+//console.log(productId)
+//var productId = '9000'
+// Test3 - NaN
+//var total = price * quantity
+//var price  = 3.00;
+//var quantity = 5;
+//console.log(total)
+//Test4 - showing a product 
+showProduct();
+function showProduct(){
+	console.log('showing a product')
+}
+// Test5-TypeError: undefined is not a function
+//showProduct();
+//var showProduct = function(){
+// 	console.log('showing a product')
+//}	
