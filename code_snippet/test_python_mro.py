f1ce1bf42760f9755a22dfcef811fc26c7bdb8aa
weiwongfaye <weiwongfaye@gmail.com>
2017-02-20 15:42:28 +0800

    add mro case

---
+++ code_snippet/test_python_mro.py
@@ -0,0 +1,13 @@
+class A(object):
+	def show(self):
+		print "A.show()"
+	def get_final_show(self):
+		self.show()
+class B(A): pass
+class C(A):
+	def show(self):
+		print "C.show()"
+class D(B, C): pass
+print D.__mro__
+d = D()
+d.get_final_show()
