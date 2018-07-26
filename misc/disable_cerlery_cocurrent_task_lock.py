53c25a0abdc3a4f4d3e147be6beb1aabe0ef1205
weiwongfaye <weiwongfaye@gmail.com>
2018-07-26 10:06:16 +0800

    celery lock

---
+++ misc/disable_cerlery_cocurrent_task_lock.py
@@ -0,0 +1,25 @@
+import redis
+import logging
+from functools import wraps
+REDIS_CLIENT = redis.Redis()
+logger = logging.getLogger(__name__)
+
+def single_instance_task(timeout=60*30):
+    def task_exc(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            have_lock = False
+            key = "key"
+            lock = REDIS_CLIENT.lock(key, timeout=timeout)
+            try:
+                have_lock = lock.acquire(blocking=False)
+                if have_lock:
+                    logger.info("accquire lock")
+                    func(*args, **kwargs)
+            finally:
+                if have_lock:
+                    lock.release()
+                    logger.info("release lock")
+
+        return wrapper
+    return task_exc
